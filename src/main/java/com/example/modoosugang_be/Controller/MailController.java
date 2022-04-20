c86b5b9d352f03768ba2ce2c0dac864657f039fd
plooox <insukim97@gmail.com>
2022-04-20 18:12:14 +0900

    [✨ feature] 증원신청 메일 기능 구현 - DB에서 교수들에게 메일 돌리기 기능 구현 - Request가 0인 경우 고려

--- src/main/java/com/example/modoosugang_be/Controller/MailController.java
+++ src/main/java/com/example/modoosugang_be/Controller/MailController.java
@@ -28,29 +28,35 @@ public class MailController {
     public void handleRequesting(){
         List<Professor> professorList = professorService.callProfessorList();
 
-        for (Professor professor : professorList) {
-            System.out.println(professor.getId());
-            List<Request> requests = requestingService.callRequestByProfessor(professor.getId());
-            Map<String, int[]> requestCounter = new HashMap<>();
+        if (professorList.size() > 0) {
+            for (Professor professor : professorList) {
+                System.out.println(professor.getId());
+                List<Request> requests = requestingService.callRequestByProfessor(professor.getId());
 
-            for (Request request : requests) {
-                System.out.println(request.getLecture());
-                int[] prevVal = requestCounter.get(request.getLecture());
-                if (prevVal == null) {
-                    requestCounter.put(request.getLecture(), new int[]{1});
-                } else {
-                    prevVal[0]++;
+                if (requests.size() > 0) {
+                    Map<String, int[]> requestCounter = new HashMap<>();
+
+                    for (Request request : requests) {
+                        System.out.println(request.getLecture());
+                        int[] prevVal = requestCounter.get(request.getLecture());
+                        if (prevVal == null) {
+                            requestCounter.put(request.getLecture(), new int[]{1});
+                        } else {
+                            prevVal[0]++;
+                        }
+                    }
+
+                    String EmailContext = "다음과 같은 증원 신청 요청이 있습니다.\n";
+                    for (String key : requestCounter.keySet()) {
+                        EmailContext = EmailContext + key+": "+requestCounter.get(key)[0] + "명\n";
+                    }
+                    System.out.println("Email: "+professor.getEmail());
+                    System.out.println("Content: "+EmailContext);
+                    sendMail(professor.getEmail(), EmailContext);
                 }
             }
-
-            String EmailContext = "다음과 같은 증원 신청 요청이 있습니다.\n";
-            for (String key : requestCounter.keySet()) {
-                EmailContext = EmailContext + key+": "+requestCounter.get(key)[0] + "명\n";
-            }
-            System.out.println("Email: "+professor.getEmail());
-            System.out.println("Content: "+EmailContext);
-            sendMail(professor.getEmail(), EmailContext);
         }
+
     }
 
 
