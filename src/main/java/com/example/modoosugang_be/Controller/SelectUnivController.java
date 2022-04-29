878be23264eeb6274a9c332991df7e2d79575f53
plooox <insukim97@gmail.com>
2022-04-29 17:25:34 +0900

    [🚑️ Hotfix] /api/home/univ 수정

--- src/main/java/com/example/modoosugang_be/Controller/SelectUnivController.java
+++ src/main/java/com/example/modoosugang_be/Controller/SelectUnivController.java
@@ -3,6 +3,7 @@ package com.example.modoosugang_be.Controller;
 import com.example.modoosugang_be.Domain.University;
 import com.example.modoosugang_be.Service.UniversityService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class SelectUnivController {
     @RestController
     public class univ_list {
 
-        @PostMapping("/home/univ")
+        @GetMapping("/home/univ")
         public ArrayList<University> test1() {
 
             return universityService.findUniversity();
