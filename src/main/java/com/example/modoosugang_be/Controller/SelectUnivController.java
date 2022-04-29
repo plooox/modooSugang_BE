7b3c3ee64f573ff2c1592ec79d199fab6feb1012
plooox <insukim97@gmail.com>
2022-04-29 18:49:55 +0900

    [🚑️ Hotfix] /api/home/univ 수정

--- src/main/java/com/example/modoosugang_be/Controller/SelectUnivController.java
+++ src/main/java/com/example/modoosugang_be/Controller/SelectUnivController.java
@@ -14,8 +14,6 @@ public class SelectUnivController {
 
     private final UniversityService universityService;
 
-
-
     @GetMapping("/home/univ")
     public ArrayList<University> test1() {
         return universityService.findUniversity();
